4798b028e91f356840f5cb3f230195015ad58755
victor-dalosto <victordalosto@gmail.com>
2025-05-20 23:17:22 -0300

    Small fixes para deixar api result imutavel

---
+++ src/main/kotlin/dnit/commons/api/PageMetaData.kt
@@ -0,0 +1,25 @@
+package dnit.commons.api
+
+
+/**
+ * Classe para armazenar metadados de paginação
+ *
+ * @property count Número total de itens na página
+ * @property total Número total de itens disponíveis
+ * @property pages Número total de páginas disponíveis
+ * @property currentPage Página atual
+ */
+class PageMetadata {
+
+    var count : Int = 0;
+    var total : Int = 0;
+    var pages : Int = 0;
+    var currentPage : Int = 0;
+
+    val hasNextPage : Boolean
+        get() = currentPage < total
+
+    val hasPreviousPage : Boolean
+        get() = currentPage > 0
+
+}
